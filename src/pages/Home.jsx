0788ae3bfd19276ebec5e2c3bddcc8b35f4bbe99
Frostbowl <masclet.c@gmail.com>
2024-06-23 18:19:28 +0400

    preparation homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/style/home.css";
+
+const Home =() =>{
+    return(
+        <div className="home">
+            <div className="container">
+                <section className="leftSide">
+                    <div className="card">
+                        <div className="card-header">
+                            <h2>Valérie Esther</h2>
+                        </div>
+                        <img src="../img/photoporo.jpg" alt="Valérie Esther" className="profilPic" />
+                    </div>
+                </section>
+                <section className="rightSide">
+                    <h1>Valérie </h1><br />
+                    <h2>Professionnelle de la coiffure depuis x années, vous accueille dans son salon du "horraires d'ouvertures"</h2><br />
+                    <h2>Venez profiter, le temps d'une prestation, d'un cadre agréable et chaleureux.</h2><br />
+                    <h3>A très vite, chez VE Coiffure et esthétique !</h3><br />
+
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
