a3520601756bf2a34a35e32677d89cebe6a8fa7f
Frostbowl <masclet.c@gmail.com>
2024-06-24 20:51:39 +0400

    preparation page contact

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -13,10 +13,10 @@ const Footer =() =>{
             <div id="social">
                 <h1>Réseaux sociaux</h1>
                 <Link to='https://www.facebook.com/profile.php?id=61558369631745&mibextid=LQQJ4d&rdid=QSKq167EO6nljJ3X&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FXupEFKnEW5DxJVqX%2F%3Fmibextid%3DLQQJ4d' target="_blank">
-                    <i class="fa-brands fa-square-facebook fa-2xl"></i> Facebook
+                    <i className="fa-brands fa-square-facebook fa-2xl"></i> Facebook
                 </Link>
                 <Link to='https://www.instagram.com/ve_coiffure_esthetique?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==' target="_blank">
-                <i class="fa-brands fa-square-instagram fa-2xl"></i> Instagram
+                <i className="fa-brands fa-square-instagram fa-2xl"></i> Instagram
                 </Link>
             </div>
             <div>
@@ -30,12 +30,15 @@ const Footer =() =>{
                 <p>02.62.08.61.41 || 06.92.59.11.54</p>
                 <p></p>
             </div>
+            <div>
+                <h1>Mentions légales</h1>
+            </div>
             <div id="link">
                 <h1>Prestations</h1>
                 <Link to='/coiffure'><p>Coiffure</p></Link>
                 <Link to='/wellness'><p>Bien-être</p></Link>
                 <Link to='/'><p>Esthétique</p></Link>
-                <Link to='/'><p>Contacte</p></Link>
+                <Link to='/contact'><p>Contacte</p></Link>
             </div>
         </footer>
     )
