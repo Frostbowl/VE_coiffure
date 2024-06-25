90d36bc59def0d0183b2e6ffdf7fcc1d28241685
Frostbowl <masclet.c@gmail.com>
2024-06-25 18:50:20 +0400

    correction orthographique laura

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,8 @@ import "../assets/style/home.css";
 const Home =() =>{
     return(
         <div className="home">
-            <div className="hello">
-                <h1>Bienvenue !</h1>
-            </div>
-            <div className="container">
+            <h1 className="hello">Bienvenue !</h1>
+            <div className="container pt-4">
                 <section className="leftSide">
                     <div className="carousel slide" id="carousel">
                         <div className="carousel-inner">
@@ -38,7 +36,7 @@ const Home =() =>{
                 <section className="rightSide">
                     <h1>Valérie </h1>
                     <h2>Professionnelle de la coiffure depuis <span>x années</span>, vous accueille dans son salon 
-                    du "horraires d'ouvertures".
+                    du "horaires d'ouvertures".
                     </h2>
                     <h2>Venez profiter, le temps d'une prestation, d'un <span className="red">cadre 
                         agréable et chaleureux.</span></h2>
